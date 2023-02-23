4bd53b4dfe948c66d9289f20cd091c753f8bc9e2
Youssef <othmaniyoussef97@gmail.com>
2023-02-22 19:10:16 -0500

    ft_split.c change

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: yothmani <yothmani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 15:04:35 by yothmani          #+#    #+#             */
-/*   Updated: 2023/02/22 16:55:33 by yothmani         ###   ########.fr       */
+/*   Updated: 2023/02/22 18:20:32 by yothmani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,63 @@ static int count_words(char const *s, char c)
 {
 	int		count;
 	int		i;
-	int		checkpoint;
-
+    
     i = 0;
     count = 0;
-    checkpoint = 0;
 	while (s[i])
 	{
-		if (s[i] == c)
-			checkpoint = 0;
-		else if (!checkpoint)
-			checkpoint = 1;
-		count++;
+        if (s[i] == c){
+            count++;
+        }
+	    i++;
 	}
-	i++;
+    count++;
     return (count);
+}
+
+char* ft_cut_str(int *start, char *str, char c){
+    int i;
+    int startCopy;
+    char *new;
+
+    i = 0;
+    startCopy = *start;
+    // tamano de nuestra palabra
+    while (str[*start + i] && str[*start + i] != c)
+    {
+        i++;
+    }
+    *start = i;
+    // crear el tamano de la palabra
+    new = malloc((i + 1 )* size_of(char));
+    // salvar la pablabra
+    i = 0;
+    while (startCopy < *start)
+    {
+        new[i] = str[startCopy];
+        startCopy++;
+        i++;
+    }
+    *start++;
+    return (new);
 }
 
 char **ft_split(char const *s, char c)
 {
+    char **table;
+    int start;
+    int i;
+ 
+    i = 0;
+    table = (char **)malloc(count_words(s, c) +1 );
+    if (!table){
+        return 0;
+    }
+    while (i < count_words(s, c))
+    {
+        table[i] = ft_cut_str(&start, s, c);
+        i++;
+    }
     
 }
 
