831a2015fdc34548d9550198e8f077ac7a883dde
Youssef <othmaniyoussef97@gmail.com>
2023-02-28 15:30:41 -0500

    lstsize update

--- ft_lstsize_bonus.c
+++ ft_lstsize_bonus.c
@@ -3,24 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstsize_bonus.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yothmani <yothmani@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yothmani <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 15:35:24 by yothmani          #+#    #+#             */
-/*   Updated: 2023/02/27 17:08:52 by yothmani         ###   ########.fr       */
+/*   Updated: 2023/02/28 15:24:56 by yothmani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_lstsize(t_list *lst)
+int	ft_lstsize(t_list *lst)
 {
-    size_t  i;
-    
-    i = 0;
-    while (lst)
-    {
-    lst = lst -> next;
-    i++;
-    }
-    return (i);
+	size_t	i;
+
+	i = 0;
+	while (lst)
+	{
+		lst = lst->next;
+		i++;
+	}
+	return (i);
 }
